4cc757c632ace1ff2a2362c056a9be4774e32c3c
Henrik Sommerfeld <henrik@sommerfeld.nu>
2022-04-26 22:05:20 +0200

    Use github api to check latest version

--- latest-version-checker.js
+++ latest-version-checker.js
@@ -1,14 +1,14 @@
-import rp from "request-promise";
+import rp from 'request-promise'
 
 export async function getLatestVersion(uri) {
   try {
     const versionInfo = await rp({
-      method: "GET",
+      method: 'GET',
       uri,
-      headers: { "User-Agent": "my node script" },
-    }).json();
-    return versionInfo.name;
+      headers: { 'User-Agent': 'my node script' },
+    }).json()
+    return versionInfo.name
   } catch (error) {
-    Promise.reject(error);
+    Promise.reject(error)
   }
 }
--- main.js
+++ main.js
@@ -1,47 +1,44 @@
-import { getLatestVersion } from './latest-version-checker.js';
-import { sendPushoverNotification } from './notify.js';
-import { getLastCheckedVersion, saveLastCheckedVersion } from './localFile.js';
+import { getLatestVersion } from './latest-version-checker.js'
+import { sendPushoverNotification } from './notify.js'
+import { getLastCheckedVersion, saveLastCheckedVersion } from './localFile.js'
 
 async function main() {
   try {
-    const lastCheckedVersion = getLastCheckedVersion();
-    console.log('main -> lastCheckedVersion', lastCheckedVersion);
-    const corePromise = getLatestVersion(releasePages.core);
-    const adminLtePromise = getLatestVersion(releasePages.adminLte);
-    const ftlPromise = getLatestVersion(releasePages.ftl);
-    const [
-      latestCoreVersion,
-      latestAdminLteVersion,
-      latestFtlVersion,
-    ] = await Promise.all([corePromise, adminLtePromise, ftlPromise]);
+    const lastCheckedVersion = getLastCheckedVersion()
+    console.log('main -> lastCheckedVersion', lastCheckedVersion)
+    const corePromise = getLatestVersion(releasePages.core)
+    const adminLtePromise = getLatestVersion(releasePages.adminLte)
+    const ftlPromise = getLatestVersion(releasePages.ftl)
+    const [latestCoreVersion, latestAdminLteVersion, latestFtlVersion] =
+      await Promise.all([corePromise, adminLtePromise, ftlPromise])
     const latestVersions = {
       core: latestCoreVersion,
       adminLte: latestAdminLteVersion,
       ftl: latestFtlVersion,
-    };
+    }
     if (JSON.stringify(latestVersions) !== JSON.stringify(lastCheckedVersion)) {
       const message = `🥧 New Pihole version available
 Core: ${latestCoreVersion}
 Admin LTE: ${latestAdminLteVersion}
 FTL: ${latestFtlVersion}
-Upgrade with 'pihole -up'`;
-      sendPushoverNotification(message);
-      saveLastCheckedVersion(latestVersions);
+Upgrade with 'pihole -up'`
+      sendPushoverNotification(message)
+      saveLastCheckedVersion(latestVersions)
     } else {
-      const message = `ℹ No upgrade for Pi-hole`;
-      sendPushoverNotification(message);
+      const message = `ℹ No upgrade for Pi-hole`
+      sendPushoverNotification(message)
     }
   } catch (error) {
-    console.log('main -> error', error);
-    const message = `⚠️ Pi-hole update check failed`;
-    sendPushoverNotification(message);
+    console.log('main -> error', error)
+    const message = `⚠️ Pi-hole update check failed`
+    sendPushoverNotification(message)
   }
 }
 
 const releasePages = {
-  core: 'https://github.com/pi-hole/pi-hole/releases',
-  adminLte: 'https://github.com/pi-hole/AdminLTE/releases',
-  ftl: 'https://github.com/pi-hole/FTL/releases',
-};
+  core: 'https://api.github.com/repos/pi-hole/pi-hole/releases/latest',
+  adminLte: 'https://api.github.com/repos/pi-hole/AdminLTE/releases/latest',
+  ftl: 'https://api.github.com/repos/pi-hole/FTL/releases/latest',
+}
 
-main();
+main()
